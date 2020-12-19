5a51ffbc2756abcf6355428d95fcbe36dd353a20
Kesoyu <75736746+Kesoyu@users.noreply.github.com>
2020-12-19 22:56:33 +0100

    Update logowanie.php

--- logowanie.php
+++ logowanie.php
@@ -1,5 +1,40 @@
-<?php
-$connect=mysqli_connect("localhost","root","","strona_z_grami");
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gpoint</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Zobacz najnowsze promocje na naszej stronie !">
+    <link rel="stylesheet" href="logowanie.css" type="text/css">
 
-mysqli_close($connect);
-?>
+</head>
+<body>
+    <div id="reje">
+        <form action="logowanie.php" method="POST">
+            Podaj login<input type="text" name="login"><br>
+            Podaj hasło<input type="text" name="haslo"><br>
+        </form>
+        <a href="rejestracja.php">Jeżeli nie posiadasz konta, zarejestruj się!</a>   
+    </div>
+</body>
+
+</html>
+<?php
+            if(!empty($_POST['login']) && !empty($_POST['haslo']));
+                $login=$_POST['login'];
+                $haslo=$_POST['haslo'];
+                $connect=mysqli_connect("localhost","root","","strona_z_grami");
+                $zapytanie="INSERT INTO `użytkownicy`(`Imię`, `Nazwisko`, `Wiek`, `Stan_konta`) VALUES ('$imie','$nazwisko','$wiek',0)";
+                $zapytanie2="INSERT INTO `dane_logowania`(`Login`, `Hasło`, `E-mail`) VALUES ('$login','$haslo1','$email');";
+                $wynik=mysqli_query($connect,$zapytanie);
+                $wynik2=mysqli_query($connect,$zapytanie2);
+                if($wynik){
+                    echo '<script>alert("Zalogowano pomyślnie uwu")</script>';
+                    header("Location: logowanie.php");
+                }
+                else{
+                    echo "2";
+
+                }
+                mysqli_close($connect);
+            }
+        ?>
