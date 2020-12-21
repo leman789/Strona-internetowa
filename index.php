84a0a4aea21fd56421be2ce9a2d1894f23f594e1
leman789 <75726269+leman789@users.noreply.github.com>
2020-12-21 20:20:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,44 @@
     <meta charset="utf-8">
     <meta name="description" content="Zobacz najnowsze promocje na naszej stronie !">
     <link rel="stylesheet" href="style.css" type="text/css">
-
+    <?php 
+    $host="localhost";
+    $uzytkownik="root";
+    $haslo="";
+    $dbname="strona_z_grami";
+    $dsn="mysql:host=$host;dbname=$dbname;";
+    $PDO = new PDO($dsn,$uzytkownik,$haslo);
+    $zapytanie="SELECT id,Imię FROM `użytkownicy` LIMIT 1";
+    $wynik=$PDO->query($zapytanie);
+    foreach($wynik as $rekord){
+    }
+    ?>
 </head>
 <body>
     <div id="menu">
 
-        <a href="logowanie.html"  class="Int">
-        <div id="menu1" > 
+        <?php
+                    if($rekord[0]==1)//po zalogowaniu
+        {
+        echo "
+        <a class='Int'><div id='menu1'>
+            $rekord[1]
+        </div></a>
+        <style>
+            #menu1{
+                color: red;
+            }
+        </style>
+        ";
+        }
+            if($rekord[0]==2)//przed zalogowaniem
+            {
+            echo "<a href='logowanie.html'  class='Int'><div id='menu1'> 
             Logowanie
-        </div> 
-
-        </a>
+            </div> </a>";
+            }
+        $PDO=null;
+        ?>
         <div id="menu2" class="Tlo"></div>
         <a href="biblioteka.html" class="Int">
             <div id="menu3"> 
