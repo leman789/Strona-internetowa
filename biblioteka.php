f74a04f6d1618ab5d52cac6ba6fa816ef4dcd9ae
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-03 20:35:19 +0100

    Create biblioteka.php

---
+++ biblioteka.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gpoint</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Zobacz najnowsze promocje na naszej stronie !">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <?php 
+    $host="localhost";
+    $uzytkownik="root";
+    $haslo="";
+    $dbname="strona_z_grami";
+    $dsn="mysql:host=$host;dbname=$dbname;";
+    $PDO = new PDO($dsn,$uzytkownik,$haslo);
+    $zapytanie="SELECT id,Imie,Avatar FROM `uzytkownicy` LIMIT 1";
+    $wynik=$PDO->query($zapytanie);
+    foreach($wynik as $rekord){ 
+    }
+    ?>
+</head>
+<body>
+    <script>
+        let x=0;
+    function wysun()
+        {
+            x++;
+            if(x==1){
+                    document.getElementById("elwys1").innerHTML="<div id='elwys'><a href='ustawienia'>ustawienia</a><br><a href='doladuj'>doladuj</a><br><a href='wyloguj.php'>wyloguj</a><br></div>";
+                }
+            else if(x>1){
+                    x=0;
+                    document.getElementById("elwys1").innerHTML="";
+                }
+        }
+    </script>
+    <div id="elwys1"></div>
+    <div id="menu">
+        <?php
+        if(isset($_COOKIE["Clogin"]))//logowanie po zalogowaniu
+        {
+        echo "<a id='Zalogowany' data-tooltip='xd'>
+        <div >
+            <img src='Zdjecia_gier/avatar/$rekord[2]' alt='awatar' width='60px'>
+            $rekord[1]
+            <input type='button' onclick='wysun()' id='wysun'>
+        </div>
+        </a>";
+        }
+        
+        else//przed zalogowaniem
+            {
+            echo "<a href='logowanie.php'  id='Zaloguj'><div> 
+            Logowanie
+            </div> </a>";
+            }
+        ?>
+       
+        
+        <div id="Puste_pole"></div>
+        
+        <?php
+        if(isset($_COOKIE["Clogin"]))//biblioteka po zalogowaniu
+        {
+        echo "<a href='biblioteka.php' id='Biblioteka'>
+            <div> 
+                Biblioteka
+            </div>
+            </a>";
+        }
+        else
+           {
+        echo "<a href='logowanie.php' id='Biblioteka'>
+            <div> 
+                Biblioteka
+            </div>
+            </a>";
+        } 
+        ?>
+        
+        <?php
+        if(isset($_COOKIE["Clogin"]))//sklep po zalogowaniu
+        {
+        echo "<a href='sklep.php' id='Sklep'>
+        <div>
+           Sklep
+        </div>
+        </a>";
+        }
+        else
+         {
+        echo "<a href='logowanie.php' id='Sklep'>
+        <div>
+           Sklep
+        </div>
+        </a>";
+        }
+        ?>
+        <div id="Puste_pole" class="Tlo"></div>
+               <a href="index.php">
+        <div id="Logo">
+            Logo
+        </div>
+        </a>  
+
+    </div>
+    
+</body>
+
+</html>
+<?php
+$login=$_COOKIE["Clogin"];
+$connect=mysqli_connect("localhost","root","","strona_z_grami");
+$id_uzytkownika="SELECT id_uzytkownika FROM `dane_logowania` WHERE Login='$login'";
+$id_uzytkownika_W=mysqli_query($connect,$id_uzytkownika);
+$id_uzytkownika_R=mysqli_fetch_array($id_uzytkownika_W);
+$biblioteka="SELECT gry.id,Nazwa,Obrazek,Alt_obrazka FROM `gry` JOIN biblioteka_gier ON biblioteka_gier.id_gry=gry.id JOIN uzytkownicy ON uzytkownicy.id=biblioteka_gier.id_uzytkownika WHERE biblioteka_gier.id_uzytkownika='$id_uzytkownika_R[0]'";
+$biblioteka_W=mysqli_query($connect,$biblioteka);
+while($biblioteka_R=mysqli_fetch_array($biblioteka_W))
+{
+    echo $biblioteka_R[0],$biblioteka_R[1],$biblioteka_R[2],$biblioteka_R[3]."<br>";
+}
+
+?>
