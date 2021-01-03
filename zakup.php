7a3cd1946ce5077a1af5b4a7c6210b6042160d8c
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-03 20:35:27 +0100

    Update zakup.php

--- zakup.php
+++ zakup.php
@@ -1,3 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gpoint</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Zobacz najnowsze promocje na naszej stronie !">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <?php 
+    $host="localhost";
+    $uzytkownik="root";
+    $haslo="";
+    $dbname="strona_z_grami";
+    $dsn="mysql:host=$host;dbname=$dbname;";
+    $PDO = new PDO($dsn,$uzytkownik,$haslo);
+    $zapytanie="SELECT id,Imie,Avatar FROM `uzytkownicy` LIMIT 1";
+    $wynik=$PDO->query($zapytanie);
+    foreach($wynik as $rekord){ 
+    }
+    ?>
+</head>
+<body>
+    <script>
+        let x=0;
+    function wysun()
+        {
+            x++;
+            if(x==1){
+                    document.getElementById("elwys1").innerHTML="<div id='elwys'><a href='ustawienia'>ustawienia</a><br><a href='doladuj'>doladuj</a><br><a href='wyloguj.php'>wyloguj</a><br></div>";
+                }
+            else if(x>1){
+                    x=0;
+                    document.getElementById("elwys1").innerHTML="";
+                }
+        }
+    </script>
+    <div id="elwys1"></div>
+    <div id="menu">
+        <?php
+        if(isset($_COOKIE["Clogin"]))//logowanie po zalogowaniu
+        {
+        echo "<a id='Zalogowany' data-tooltip='xd'>
+        <div >
+            <img src='Zdjecia_gier/avatar/$rekord[2]' alt='awatar' width='60px'>
+            $rekord[1]
+            <input type='button' onclick='wysun()' id='wysun'>
+        </div>
+        </a>";
+        }
+        
+        else//przed zalogowaniem
+            {
+            echo "<a href='logowanie.php'  id='Zaloguj'><div> 
+            Logowanie
+            </div> </a>";
+            }
+        ?>
+       
+        
+        <div id="Puste_pole"></div>
+        
+        <?php
+        if(isset($_COOKIE["Clogin"]))//biblioteka po zalogowaniu
+        {
+        echo "<a href='biblioteka.php' id='Biblioteka'>
+            <div> 
+                Biblioteka
+            </div>
+            </a>";
+        }
+        else
+           {
+        echo "<a href='logowanie.php' id='Biblioteka'>
+            <div> 
+                Biblioteka
+            </div>
+            </a>";
+        } 
+        ?>
+        
+        <?php
+        if(isset($_COOKIE["Clogin"]))//sklep po zalogowaniu
+        {
+        echo "<a href='sklep.php' id='Sklep'>
+        <div>
+           Sklep
+        </div>
+        </a>";
+        }
+        else
+         {
+        echo "<a href='logowanie.php' id='Sklep'>
+        <div>
+           Sklep
+        </div>
+        </a>";
+        }
+        ?>
+        <div id="Puste_pole" class="Tlo"></div>
+        <a href="index.php">
+        <div id="Logo">
+            Logo
+        </div>
+        </a>  
+    </div>   
+</body>
+</html>
 <?php
 $login=$_COOKIE["Clogin"];
 $id=$_POST['id'];
