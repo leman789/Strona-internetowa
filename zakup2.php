000498dba8bd5070e58eadfa753ffcdec321a9a8
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-03 20:35:29 +0100

    Update zakup2.php

--- zakup2.php
+++ zakup2.php
@@ -1,17 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gpoint</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Zobacz najnowsze promocje na naszej stronie !">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <?php 
+    $host="localhost";
+    $uzytkownik="root";
+    $haslo="";
+    $dbname="strona_z_grami";
+    $dsn="mysql:host=$host;dbname=$dbname;";
+    $PDO = new PDO($dsn,$uzytkownik,$haslo);
+    $zapytanie="SELECT id,Imie,Avatar FROM `uzytkownicy` LIMIT 1";
+    $wynik=$PDO->query($zapytanie);
+    foreach($wynik as $rekord){ 
+    }
+    ?>
+</head>
+<body>
+    <script>
+        let x=0;
+    function wysun()
+        {
+            x++;
+            if(x==1){
+                    document.getElementById("elwys1").innerHTML="<div id='elwys'><a href='ustawienia'>ustawienia</a><br><a href='doladuj'>doladuj</a><br><a href='wyloguj.php'>wyloguj</a><br></div>";
+                }
+            else if(x>1){
+                    x=0;
+                    document.getElementById("elwys1").innerHTML="";
+                }
+        }
+    </script>
+    <div id="elwys1"></div>
+    <div id="menu">
+        <?php
+        if(isset($_COOKIE["Clogin"]))//logowanie po zalogowaniu
+        {
+        echo "<a id='Zalogowany' data-tooltip='xd'>
+        <div >
+            <img src='Zdjecia_gier/avatar/$rekord[2]' alt='awatar' width='60px'>
+            $rekord[1]
+            <input type='button' onclick='wysun()' id='wysun'>
+        </div>
+        </a>";
+        }
+        
+        else//przed zalogowaniem
+            {
+            echo "<a href='logowanie.php'  id='Zaloguj'><div> 
+            Logowanie
+            </div> </a>";
+            }
+        ?>
+       
+        
+        <div id="Puste_pole"></div>
+        
+        <?php
+        if(isset($_COOKIE["Clogin"]))//biblioteka po zalogowaniu
+        {
+        echo "<a href='biblioteka.php' id='Biblioteka'>
+            <div> 
+                Biblioteka
+            </div>
+            </a>";
+        }
+        else
+           {
+        echo "<a href='logowanie.php' id='Biblioteka'>
+            <div> 
+                Biblioteka
+            </div>
+            </a>";
+        } 
+        ?>
+        
+        <?php
+        if(isset($_COOKIE["Clogin"]))//sklep po zalogowaniu
+        {
+        echo "<a href='sklep.php' id='Sklep'>
+        <div>
+           Sklep
+        </div>
+        </a>";
+        }
+        else
+         {
+        echo "<a href='logowanie.php' id='Sklep'>
+        <div>
+           Sklep
+        </div>
+        </a>";
+        }
+        ?>
+        <div id="Puste_pole" class="Tlo"></div>
+        <a href="index.php">
+        <div id="Logo">
+            Logo
+        </div>
+        </a>  
+    </div>   
+</body>
+</html>
 <?php
 $connect=mysqli_connect("localhost","root","","strona_z_grami");
 $gra=$_POST['gra'];
 $login=$_COOKIE["Clogin"];
-$uzytkownik="SELECT id_uzytkownika FROM `dane_logowania` WHERE Login='$login'";
+
+$uzytkownik="SELECT id_uzytkownika FROM `dane_logowania` WHERE Login='$login'";//pobieranie id uzytkownika
 $wynik=mysqli_query($connect,$uzytkownik);
 $rekord=mysqli_fetch_array($wynik);
-$stan_konta="SELECT dane_logowania.Login,uzytkownicy.Stan_konta FROM `uzytkownicy` JOIN dane_logowania ON uzytkownicy.id=dane_logowania.id_uzytkownika where Login='$login'";
+
+$stan_konta="SELECT dane_logowania.Login,uzytkownicy.Stan_konta FROM `uzytkownicy` JOIN dane_logowania ON uzytkownicy.id=dane_logowania.id_uzytkownika where Login='$login'";//pobieranie stanu konta uzytkownika
 $wynik2=mysqli_query($connect,$stan_konta);
 $rekord2=mysqli_fetch_array($wynik2);
-$cena_gry="SELECT Cena FROM `gry` WHERE id=2";
+
+$cena_gry="SELECT Cena FROM `gry` WHERE id=2";//pobieranie ceny gry
 $wynik3=mysqli_query($connect,$cena_gry);
 $rekord3=mysqli_fetch_array($wynik3);
-$posiadanie_gry="SELECT id_gry,id_uzytkownika FROM `biblioteka_gier` WHERE id_uzytkownika='$rekord[0]' AND id_gry='$gra'";
+
+$posiadanie_gry="SELECT id_gry,id_uzytkownika FROM `biblioteka_gier` WHERE id_uzytkownika='$rekord[0]' AND id_gry='$gra'";//pobieranie zakupionych gier uzytkownika
 $wynik10=mysqli_query($connect,$posiadanie_gry);
 $rekord10=mysqli_fetch_array($wynik10);
 
@@ -27,8 +137,8 @@ if($rekord2[1]>=$rekord3[0])
 {
   $dodawanie_do_biblioteki="INSERT INTO `biblioteka_gier` (`id`, `id_gry`, `id_uzytkownika`) VALUES (NULL, '$gra', '$rekord[0]');";
       $wynik7=mysqli_query($connect,$dodawanie_do_biblioteki);
-  $podpierdalanie_kasy="UPDATE `uzytkownicy` SET `Stan_konta` = `Stan_konta`-$rekord3[0] WHERE `uzytkownicy`.`id` = '$rekord[0]';";
-      $wynik8=mysqli_query($connect,$podpierdalanie_kasy);
+  $pobieranie_oplaty="UPDATE `uzytkownicy` SET `Stan_konta` = `Stan_konta`-$rekord3[0] WHERE `uzytkownicy`.`id` = '$rekord[0]';";
+      $wynik8=mysqli_query($connect,$pobieranie_oplaty);
     header("Location:sklep.php");
 }
 else{
