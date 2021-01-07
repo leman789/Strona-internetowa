4bd32bc403cafb48579ebdbb7a0b6039057e9ac9
r3czus <76210775+r3czus@users.noreply.github.com>
2021-01-08 00:10:58 +0100

    Update dane_osobowe.php

--- ustawienia/dane_osobowe.php
+++ ustawienia/dane_osobowe.php
@@ -6,6 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="description" content="Zobacz najnowsze promocje na naszej stronie !">
     <link rel="stylesheet" href="../style.css" type="text/css">
+    <link rel="stylesheet" href="../menu.css" type="text/css">
     <?php 
     $host="localhost";
     $uzytkownik="root";
