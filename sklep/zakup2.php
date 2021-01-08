8777cb668ad3e075b565f33ef0b44c72570723e5
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-08 15:33:30 +0100

    Update zakup2.php

--- sklep/zakup2.php
+++ sklep/zakup2.php
@@ -20,6 +20,9 @@
     $wynik=$PDO->query($zapytanie);
     foreach($wynik as $rekord){ 
     }
+       $stan_konta_elwys="SELECT `Stan_konta` FROM `uzytkownicy` WHERE `id` = $rekord[0]";
+    $stan_konta_elwys_w=$PDO->query($stan_konta_elwys);
+    foreach($stan_konta_elwys_w as $stan_konta_elwys_r)
     ?>
 </head>
 <body>
@@ -29,7 +32,7 @@
         {
             x++;
             if(x==1){
-                    document.getElementById("elwys1").innerHTML="<div id='elwys'><a href='ustawienia'>ustawienia</a><br><a href='doladuj'>doladuj</a><br><a href='wyloguj.php'>wyloguj</a><br></div>";
+                    document.getElementById("elwys1").innerHTML="<div id='elwys'><?php echo "<a id='elwys2'> Stan Konta:".$stan_konta_elwys_r[0]."zł</a><br>" ?><a href='../ustawienia/dane_osobowe.php' id='elwys2'>ustawienia</a><br><a href='../wyloguj.php' id='elwys2'>wyloguj</a><br></div>";
                 }
             else if(x>1){
                     x=0;
@@ -43,9 +46,11 @@
         if(isset($_COOKIE["Clogin"]))//logowanie po zalogowaniu
         {
         echo "<a id='Zalogowany' data-tooltip='xd'>
-        <div >
-            <img src='Zdjecia_gier/avatar/$rekord[2]' alt='awatar' width='60px'>
-            $rekord[1]
+         <div id='nick'>
+         $rekord[1]
+        </div>
+        <div>
+            <img src='../Zdjecia_gier/avatar/$rekord[2]' alt='awatar' width='45px'>
             <input type='button' onclick='wysun()' id='wysun'>
         </div>
         </a>";
@@ -53,7 +58,7 @@
         
         else//przed zalogowaniem
             {
-            echo "<a href='logowanie.php'  id='Zaloguj'><div> 
+            echo "<a href='../logowanie.php'  id='Zaloguj'><div> 
             Logowanie
             </div> </a>";
             }
@@ -65,7 +70,7 @@
         <?php
         if(isset($_COOKIE["Clogin"]))//biblioteka po zalogowaniu
         {
-        echo "<a href='biblioteka.php' id='Biblioteka'>
+        echo "<a href='../biblioteka.php' id='Biblioteka'>
             <div> 
                 Biblioteka
             </div>
@@ -73,7 +78,7 @@
         }
         else
            {
-        echo "<a href='logowanie.php' id='Biblioteka'>
+        echo "<a href='../logowanie.php' id='Biblioteka'>
             <div> 
                 Biblioteka
             </div>
@@ -84,7 +89,7 @@
         <?php
         if(isset($_COOKIE["Clogin"]))//sklep po zalogowaniu
         {
-        echo "<a href='sklep.php' id='Sklep'>
+        echo "<a href='../sklep.php' id='Sklep'>
         <div>
            Sklep
         </div>
@@ -92,7 +97,7 @@
         }
         else
          {
-        echo "<a href='logowanie.php' id='Sklep'>
+        echo "<a href='../logowanie.php' id='Sklep'>
         <div>
            Sklep
         </div>
@@ -100,15 +105,14 @@
         }
         ?>
         <div id="Puste_pole" class="Tlo"></div>
-        <a href="index.php">
+        <a href="../index.php">
         <div id="Logo">
             Logo
         </div>
         </a>  
-    </div>   
-</body>
-</html>
-<?php
+    </div>
+    <div id="content">
+    <?php
 $connect=mysqli_connect("localhost","root","","strona_z_grami");
 $gra=$_POST['gra'];
 $login=$_COOKIE["Clogin"];
@@ -155,3 +159,6 @@ else{
     echo "masz juz ta gre";
 }
 ?>
+    </div>
+</body>
+</html>
