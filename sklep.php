80bc3ae0b8270653ff35b9422dafe4e01f6c4834
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-13 21:52:11 +0100

    Update sklep.php

--- sklep.php
+++ sklep.php
@@ -14,14 +14,21 @@
     $dbname="strona_z_grami";
     $dsn="mysql:host=$host;dbname=$dbname;";
     $PDO = new PDO($dsn,$uzytkownik,$haslo);
-    $zapytanie="SELECT id,Imie,Avatar FROM `uzytkownicy` LIMIT 1";
+    
+    //pobieranie id z loginu
+    $login=$_COOKIE["Clogin"];
+    $id_uzytkownika="SELECT id_uzytkownika FROM `dane_logowania` WHERE Login='$login'";
+    $id_uzytkownika_W=$PDO->query($id_uzytkownika);
+    foreach($id_uzytkownika_W as $id_uzytkownika_R)
+    //zapytanie
+    $zapytanie="SELECT id,Imie,Avatar FROM `uzytkownicy` where id='$id_uzytkownika_R[0]'";
     $wynik=$PDO->query($zapytanie);
     foreach($wynik as $rekord){ 
     }
+    //stan konta uzytkownika
     $stan_konta_elwys="SELECT `Stan_konta` FROM `uzytkownicy` WHERE `id` = $rekord[0]";
     $stan_konta_elwys_w=$PDO->query($stan_konta_elwys);
     foreach($stan_konta_elwys_w as $stan_konta_elwys_r)
-   
     ?>
 </head>
 <body>
