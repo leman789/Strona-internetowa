f97ef3b865c0662a5628f7604147789082b0c5a0
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-18 15:38:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -116,14 +116,24 @@
         </div>
         </a>  
     </div>
-    
+            <?php
+        if(isset($_COOKIE["Clogin"])){//sklep po zalogowaniu
+            echo "<div id='przejscie1'></div>";
+        }
+        else{
+            echo "<a href='logowanie.php' id='Sklep'>
+            <div id='przejscie'>
+            DOWIEDZ SIĘ WIĘCEJ
+            </div>
+            </a>";
+        }?>
   <img  id="obraz1" src="Zdjecia_gier/Top_gry/1.jpg" alt="zd_glowne">
   <div id="napis"></div>
+  <div id="napis2"></div>
     <h2>ZOBACZ WIĘCEJ GIER </h2>
     <input type="button"  onclick="dodaj()" id="Kolejne_zdj">
     <input type="button"  onclick="odejmij()" id="Poprzednie_zdj">
-    <script  src="zmianazdjzawartosc.js" defer></script><!--dodanie pliku z zmianą obrazu i zawartosc-->
-       
-    
+    <script  src="zmianazdjzawartosc.js" defer>
+    </script><!--dodanie pliku z zmianą obrazu i zawartosc-->
 </body>
 </html>
