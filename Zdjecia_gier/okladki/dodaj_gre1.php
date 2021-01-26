5d1dc89eb5195861d661be1a587fad37491b5d9e
r3czus <76210775+r3czus@users.noreply.github.com>
2021-01-26 17:22:09 +0100

    Create dodaj_gre1.php

---
+++ Zdjecia_gier/okladki/dodaj_gre1.php
@@ -0,0 +1,32 @@
+<?php
+
+    $max_rozmiar = 1024*1024;
+    if(is_uploaded_file($_FILES['plik']['tmp_name']))
+    {
+        if($_FILES['plik']['size'] > $max_rozmiar)
+        {
+            echo "za duży plik";
+        }
+        else
+        {
+            $nazwa=$_FILES['plik']['name'];
+           
+            if(isset($_FILES['plik']['type']))
+            {
+               
+            }
+            move_uploaded_file($_FILES['plik']['tmp_name'],$_SERVER['DOCUMENT_ROOT'].'/git/Strona-internetowa/Zdjecia_gier/okladki/'.$_FILES['plik']['name']);
+        }
+    }
+    else
+    {
+        echo "błąd przy przesyłaniu danych";
+    }
+    echo "<img src='$nazwa' alt='zdj_avatara'>";
+ $login=$_COOKIE["Clogin"];
+$connect=mysqli_connect("localhost","root","","strona_z_grami");
+$id_uzytkownika="SELECT id_uzytkownika FROM `dane_logowania` WHERE Login='$login'";
+$id_uzytkownika_W=mysqli_query($connect,$id_uzytkownika);
+$id_uzytkownika_R=mysqli_fetch_array($id_uzytkownika_W);
+//wyswitlanie rekordow 
+ echo "dziala";
