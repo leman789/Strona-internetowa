497a2c781a20924f8beb49c4b0a6b688fec9b0f1
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-27 13:21:30 +0100

    Update dodaj_gre.php

--- ustawienia/dodaj_gre.php
+++ ustawienia/dodaj_gre.php
@@ -20,28 +20,26 @@
         <p id="wynik"></p>
 <div id="prawa1">
 <?php 
- echo "<form action='../Zdjecia_gier/okladki/dodaj_gre1.php' method='POST' ENCTYPE='multipart/form-data'>";
- echo  "<scroll-container><div id='prawa_1'>";
+ echo "<form action='../Zdjecia_gier/okladki/dodaj_gre1.php' method='POST' ENCTYPE='multipart/form-data' runat='server'> ";
+ echo  "<div id='prawa_1'>";
   echo '<h2>Dodaj włąsną grę</h2>';
  echo '<div>Nazwa gry:</div><input type="text" name="tytul"><br>';
   echo '<div>Cena:</div> <input type="number" name="cena"><br>';
   echo '<div>Data wydania</div> <input type="date" name="data"><br><br>';
   echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>';
   echo   "<div>Dodaj okładke:<br> </div><div class='file-input'>
-        <form runat='server'>
        <input type='file' name='plik' id='file' class='file'>
        <label for='file'>
          Wybierz plik ...
          <p class='file-name'></p>
        </label>
        <img id='blah' src='#' alt='your image'  width='250px'/>
-       </form>
      </div>
         <br><br><br>";      
   echo '<div>Napisz opisz swojej gry do 250 znakow:</div><br>';
  echo' <textarea name="opis" id="" cols="80" rows="5"></textarea><br>';
     echo '<input type="button" value="dalej" onClick="prawa1_dalej()" class="przycisk">';
-    echo   "</div></scroll-container>";
+    echo   "</div>";
  echo  "<div id='prawa_2'>";
          echo '<h2>Dodaj włąsną grę</h2>';
         echo '<h3>Twórca gry</h3>';
@@ -61,13 +59,9 @@
       echo '<div>Miejsce na dysku:</div> <input type="number" name="Wydawca"><br>';
       echo '<div>DirectX:</div> <input type="number" name="Wydawca"><br>';
     echo '<input type="button" value="wstecz" onClick="prawa3_wstecz()" class="przycisk">';
-        echo " <input type='submit' value='zapisz' id='zmien1' class='przycisk'></form>";
+        echo '<input type="submit" value="zapisz" id="zmien1" class="przycisk"></form>';
     
  echo   "</div>";
-
- 
-
-echo '</form>';
 ?>
 <script>
  function readURL(input) {
