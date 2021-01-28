884f47dab999e3e8917c08e79acd58971a961efd
leman789 <75726269+leman789@users.noreply.github.com>
2021-01-28 17:08:07 +0100

    Update panel_banowania.php

--- ustawienia/panel_banowania.php
+++ ustawienia/panel_banowania.php
@@ -34,26 +34,29 @@ $login=$_COOKIE["Clogin"];
 $connect=mysqli_connect("localhost","root","","strona_z_grami");
 ?>
 <?php 
-    echo "<form action='../Zdjecia_gier/okladki/dodaj_gre1.php' method='POST' ENCTYPE='multipart/form-data' runat='server'> ";
-    
-    
+    //początek menu
     echo  "<div id='prawa_1'>";
     echo '<input type="button" value="Gracze" onClick="prawa1_dalej()" class="prawa_1_przycisk">';
-    echo '<input type="button" value="Gry" onClick="prawa2_dalej()" class="prawa_1_przycisk">';
+    echo '<input type="button" value="Gry" onClick="prawa2_dalej()" id="Przycisk_gry" class="prawa_1_przycisk">';
     echo   "</div>";
     
-    
+    //Ban osoby
     echo  "<div id='prawa_2'>";
-    echo 'gracze';
+    echo  "<form action='Ban_osoby.php' method='post'>";
+    echo '<p>Login osoby do zbanowania:</p><br> <input type="text" name="login_ofiary"><br>';
+    echo '<p>Powod:</p><br> <textarea name="Powod_osoby" id="" cols="30" rows="3" maxlength="100"></textarea><br>';
     echo '<input type="button" value="wstecz" onClick="prawa2_wstecz()" class="przycisk">';
-    echo '<input type="submit" value="zapisz" id="zmien1" class="przycisk"></form>'; 
+    echo '<input type="submit" value="zbanuj" id="zmien1" class="przycisk"></form>'; 
+    
     echo   "</div>";
 
-    
+    //Ban gry
     echo  "<div id='prawa_3'>";
-    echo 'gry';
+    echo  "<form action='Ban_gry.php' method='post'>";
+    echo '<p>Nazwa gry do zbanowania:</p><br> <input type="text" name="Nazwa_gry"><br>';
+    echo '<p>Powod:</p><br> <textarea name="Powod_gry" id="" cols="30" rows="3" maxlength="100"></textarea><br>';
     echo '<input type="button" value="wstecz" onClick="prawa2_wstecz()" class="przycisk">';
-    echo '<input type="submit" value="zapisz" id="zmien1" class="przycisk"></form>';
+    echo '<input type="submit" value="zbanuj" id="zmien1" class="przycisk"></form>';
     
     
  echo   "</div>";
@@ -65,7 +68,7 @@ mysqli_close($connect);
     function prawa1_dalej(){
      document.getElementById("wynik").innerHTML="<style>#prawa_1{z-index: 1;}#prawa_2{z-index: 5;} #prawa_3{z-index: 1;}</style>";}
     function prawa2_dalej(){
-     document.getElementById("wynik").innerHTML="<style>#prawa_1{z-index: 1;}#prawa_2{z-index: 1;} #prawa_3{z-index: 5;}</style>";}
+     document.getElementById("wynik").innerHTML="<style>#prawa_1{z-index: 2;}#prawa_2{z-index: 1;} #prawa_3{z-index: 5;}</style>";}
     function prawa2_wstecz(){
          document.getElementById("wynik").innerHTML="<style>#prawa_1{z-index: 5;}#prawa_2{z-index: 1;} #prawa_3{z-index: 1;}</style>";}
     function prawa3_wstecz(){
