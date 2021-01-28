d1df30c0b5e151579c7276651a863cf0f27dd4b6
r3czus <76210775+r3czus@users.noreply.github.com>
2021-01-28 11:13:14 +0100

    Update dodaj_gre1.php

--- Zdjecia_gier/okladki/dodaj_gre1.php
+++ Zdjecia_gier/okladki/dodaj_gre1.php
@@ -10,6 +10,8 @@ $Procesor=$_POST['Procesor'];
 $Ram=$_POST['Ram'];
 $Miejsce=$_POST['Miejsce'];
 $DirectX=$_POST['DirectX'];
+$Gatunki=$_POST['gatunki'];
+$Rodzaj_g=$_POST['rodzaj_g'];
     $max_rozmiar = 1024*1024;
     if(is_uploaded_file($_FILES['plik']['tmp_name']))
     {
@@ -33,14 +35,59 @@ $DirectX=$_POST['DirectX'];
         echo "błąd przy przesyłaniu danych";
     }
     echo "<img src='$nazwa' alt='zdj_avatara'>";
-    echo $tworca;
-    echo $wydawca;
     //id_uzytkownika
+    
  $login=$_COOKIE["Clogin"];
 $connect=mysqli_connect("localhost","root","","strona_z_grami");
 $id_uzytkownika="SELECT id_uzytkownika FROM `dane_logowania` WHERE Login='$login'";
 $id_uzytkownika_W=mysqli_query($connect,$id_uzytkownika);
 $id_uzytkownika_R=mysqli_fetch_array($id_uzytkownika_W);
+
+
+$id_gry_zapytanie="SELECT `id` FROM gry GROUP by `id` DESC LIMIT 1";
+$id_gry_zapytanie_W=mysqli_query($connect,$id_gry_zapytanie);
+$id_gry_zapytanie_R=mysqli_fetch_array($id_gry_zapytanie_W);
+//danie id
+ $id_gry=$id_gry_zapytanie_R[0]+1;
+
+
+if($Rodzaj_g=="singlep")
+ { 
+$dodanie_gatunku="INSERT INTO `gatunki_multi_single` (`id_gry`, `multip`, `singlep`) VALUES ('$id_gry', '0', '1');";
+ }
+else
+{ 
+$dodanie_gatunku="INSERT INTO `gatunki_multi_single` (`id_gry`, `multip`, `singlep`) VALUES ('$id_gry', '1', '0');";
+}
+mysqli_query($connect,$dodanie_gatunku);
+
+echo $Gatunki;
+echo $Rodzaj_g;
+echo "<br>";
+echo $id_gry;
+if($Gatunki=="fps")
+$dodanie_gatunku_gry="INSERT INTO `gatunki` (`id_gry`, `fps`, `mmo`, `rpg`, `moba`, `inne`) VALUES ('$id_gry', '1', '0', '0', '0', '0');";
+else if($Gatunki=="mmo")
+$dodanie_gatunku_gry="INSERT INTO `gatunki` (`id_gry`, `fps`, `mmo`, `rpg`, `moba`, `inne`) VALUES ('$id_gry', '0', '1', '0', '0', '0');";
+ else if($Gatunki=="rpg")
+$dodanie_gatunku_gry="INSERT INTO `gatunki` (`id_gry`, `fps`, `mmo`, `rpg`, `moba`, `inne`) VALUES ('$id_gry', '0', '0', '1', '0', '0');";
+ else if($Gatunki=="moba")
+$dodanie_gatunku_gry="INSERT INTO `gatunki` (`id_gry`, `fps`, `mmo`, `rpg`, `moba`, `inne`) VALUES ('$id_gry', '0', '0', '0', '1', '0');";
+else if ($Gatunki=="inne")
+{ 
+$dodanie_gatunku_gry="INSERT INTO `gatunki` (`id_gry`, `fps`, `mmo`, `rpg`, `moba`, `inne`) VALUES ('$id_gry', '0', '0', '0', '0', '1');";
+}
+else
+{
+    $dodanie_gatunku_gry="INSERT INTO `gatunki` (`id_gry`, `fps`, `mmo`, `rpg`, `moba`, `inne`) VALUES ('$id_gry', '0', '0', '0', '0', '1');";
+}
+mysqli_query($connect,$dodanie_gatunku_gry);
+
+
+
+
+
+
 //id_towrycy 
 $id_tworcy_zapytanie="SELECT `id` FROM `tworcy` GROUP by `id` DESC LIMIT 1";//jakie i1
 $id_tworcy_zapytanie_W=mysqli_query($connect,$id_tworcy_zapytanie);
@@ -50,20 +97,16 @@ $id_tworcy_zapytanie_R=mysqli_fetch_array($id_tworcy_zapytanie_W);
 $dodanie_tworcy="INSERT INTO `tworcy` (`id`, `Tworca`, `Wydawca`) VALUES ('$id_tworcy', '$Tworca', '$Wydawca');";
 $dodanie_tworcy_W=mysqli_query($connect,$dodanie_tworcy);
 //id gry
-$id_gry_zapytanie="SELECT `id` FROM gry GROUP by `id` DESC LIMIT 1";
-$id_gry_zapytanie_W=mysqli_query($connect,$id_gry_zapytanie);
-$id_gry_zapytanie_R=mysqli_fetch_array($id_gry_zapytanie_W);
-//danie id
- $id_gry=$id_gry_zapytanie_R[0]+1;
+
+ 
  
  $id_specyfikacja_zapytanie="SELECT `id` FROM specyfikacja GROUP by `id` DESC LIMIT 1";
  $id_specyfikacja_zapytanie_W=mysqli_query($connect,$id_specyfikacja_zapytanie);
  $id_specyfikacja_zapytanie_R=mysqli_fetch_array($id_specyfikacja_zapytanie_W);
 $id_specyfikacji=$id_specyfikacja_zapytanie_R[0]+1;
+
 $dodanie_gry="INSERT INTO `gry` (`id`, `Nazwa`, `Opis`, `Cena`, `id_tworcy`, `Data_wydania`, `Obrazek`, `Alt_obrazka`, `id_specyfikacja`, `id_dodajacego`) VALUES (NULL, '$tytul', '$opis', '$cena', '$id_tworcy', '$data', '$nazwa', '$tytul', '$id_specyfikacji', '$id_uzytkownika_R[0]');";
 $dodanie_gry_W=mysqli_query($connect,$dodanie_gry);
-
-
 
 
 
@@ -71,7 +114,6 @@ $specyfikacja_Z="INSERT INTO `specyfikacja` (`id`, `id_gry`, `system_o`, `proces
 $specyfikacja_W=mysqli_query($connect,$specyfikacja_Z);
 //danie id
  
-echo "to sjflksjfsldfkjslfkjsflkj $id_specyfikacji";
 $aktualizacja_gry="UPDATE `gry` SET `id_specyfikacja` = '$id_specyfikacji' WHERE `gry`.`id` = $id_gry;";
 $aktualizacja_gry_W=mysqli_query($connect,$aktualizacja_gry);
 
