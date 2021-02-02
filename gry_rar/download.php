42e11540ec96b1916bfb7a23bc4182d878efc71e
r3czus <76210775+r3czus@users.noreply.github.com>
2021-02-02 16:31:17 +0100

    Update download.php

--- gry_rar/download.php
+++ gry_rar/download.php
@@ -2,12 +2,12 @@
 
 if(isset($_GET['path']))
 {
-//Read the filename
+
 $filename = $_GET['path'];
-//Check the file exists or not
+
 if(file_exists($filename)) {
 
-//Define header information
+
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header("Cache-Control: no-cache, must-revalidate");
@@ -16,19 +16,18 @@ header('Content-Disposition: attachment; filename="'.basename($filename).'"');
 header('Content-Length: ' . filesize($filename));
 header('Pragma: public');
 
-//Clear system output buffer
 flush();
 
-//Read the size of the file
+
 readfile($filename);
 
-//Terminate from the script
+
 die();
 }
 else{
-echo "File does not exist.";
+echo "nie ma takiego pliku.";
 }
 }
 else
-echo "Filename is not defined."
+echo "nie zaznaczono pliku."
 ?>
