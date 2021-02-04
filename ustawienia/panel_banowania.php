2a77df237a74b6fb56ff10ca4caf958676453482
leman789 <75726269+leman789@users.noreply.github.com>
2021-02-04 14:08:38 +0100

    Update panel_banowania.php

--- ustawienia/panel_banowania.php
+++ ustawienia/panel_banowania.php
@@ -10,22 +10,7 @@
     <?php include('../menu/menu_2.php');?>
     <div style="position:absolute; top:10%;" id="content">
     <div id="lewa">
-     <a href="dane_osobowe.php"><div>Dane osobowe</div></a><br>
-     <a href="doladuj.php"><div>Doladuj konto</div></a><br>
-     <a href="transakcje.php"><div>Transakcje</div></a><br>
-     <a href="zabezpieczenia.php"><div>Haslo i zabezpieczenia</div></a><br>
-        <?php
-        if($uprawnienia[0]!=0){
-     echo "<a href='dodaj_gre.php'><div>Dodaj własną grę</div></a><br>";
-     echo "<a href='statystyki_gier.php'><div>Statystyki gier</div></a><br>";
-     echo "<a href='panel_gier.php'><div>Panel gier</div></a><br>";
-        }?>
-          <?php
-        if($uprawnienia[0]==2){
-             echo "<a href='panel_banowania.php'><div>Panel administratora</div></a><br>";
-        }
-    ?>
-        
+     <?php include('ustawienia_lewa.php');?>
     </div>
     <p id="wynik"></p>
 <div id="prawa1">
