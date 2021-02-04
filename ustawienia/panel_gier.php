8540c73aa6de02f856a6af24e7bf3facb1b8f6cb
leman789 <75726269+leman789@users.noreply.github.com>
2021-02-04 14:08:41 +0100

    Update panel_gier.php

--- ustawienia/panel_gier.php
+++ ustawienia/panel_gier.php
@@ -5,19 +5,13 @@
     
     <link rel="stylesheet" href="../style/panel_gier.css" type="text/css">
     <?php include('../menu/head_2.php');?>
+     <?php include('uprawnienia.php');?>
 </head>
 <body>
     <?php include('../menu/menu_2.php');?>
     <div style="position:absolute; top:10%;" id="content">
     <div id="lewa">
-     <a href="dane_osobowe.php"><div>Dane osobowe</div></a><br>
-     <a href="doladuj.php"><div>Doladuj konto</div></a><br>
-     <a href="transakcje.php"><div>Transakcje</div></a><br>
-     <a href="zabezpieczenia.php"><div>Haslo i zabezpieczenia</div></a><br>
-     <a href="dodaj_gre.php"><div>Dodaj własną grę</div></a><br>
-     <a href="statystyki_gier.php"><div>Statystyki gier</div></a><br>
-     <a href="panel_gier.php"><div>Panel gier</div></a><br>
-
+    <?php include('ustawienia_lewa.php');?>
     </div>
 <div id="prawa">
 <h2>Panel gier</h2>
